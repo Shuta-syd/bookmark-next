47cc22aea46dc4779bc4ea272a4bd1566d77367b
Shuta-syd <shuta.ogura0921@gmail.com>
2022-11-08 19:26:12 +0900

    [init] create nextapp bookmark

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const { isPrivateIdentifier } = require("typescript");
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: '17.0.2'
+    }
+  }
+}
