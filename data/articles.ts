91e17d95779b46ef9b0fec0f68e2a4bb1f84c90c
Shuta-syd <shuta.ogura0921@gmail.com>
2022-11-09 22:12:42 +0900

    [add] article listing page

---
+++ data/articles.ts
@@ -0,0 +1,26 @@
+export const articles = [
+  {
+    title: 'Title 1',
+    content: 'This is title 1',
+  },
+  {
+    title: 'Title 2',
+    content: 'This is title 2',
+  },
+  {
+    title: 'Title 3',
+    content: 'This is title 3',
+  },
+  {
+    title: 'Title 4',
+    content: 'This is title 4',
+  },
+  {
+    title: 'Title 5',
+    content: 'This is title 5',
+  },
+  {
+    title: 'Title 6',
+    content: 'This is title6',
+  },
+];
