3eafbb7037b51866fc95be2caa1390ffe3ad8d8b
Shuta-syd <shuta.ogura0921@gmail.com>
2022-11-09 23:03:56 +0900

    [add] bookmark function

---
+++ pages/api/bookmark/remove/[id].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../../lib/prisma';
+import { getSession } from 'next-auth/react';
+
+// リクエストとレスポンスの型指定
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  //reqのオブジェクトから認証情報を取得
+  const session = await getSession({ req });
+
+  if (session?.user?.email) {
+    const result = await prisma.article.update({
+      where: {
+        id: Number(req.query.id),
+      },
+      data: {
+        users: {
+          disconnect: { email: session?.user?.email },
+        },
+      },
+    });
+    res.json(result);
+  } else {
+    res.status(401).send({ message: 'Unauthorized' });
+  }
+};
--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,3 +1,4 @@
+import Router from "next/router";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import prisma from "../../lib/prisma";
@@ -7,6 +8,20 @@ import { ArticleProps } from "../../types/Article";
 type Props = {
   article: ArticleProps;
   isBookmarked: boolean;
+}
+
+async function addBookmark(id: number): Promise<void> {
+  await fetch(process.env.NEXT_PUBLIC_VERCEL_URL + `/api/bookmark/add/${id}`, {
+    method: 'PUT',
+  });
+  Router.push(`/articles/${id}`);
+}
+
+async function removeBookmark(id: number): Promise<void> {
+  await fetch(process.env.NEXT_PUBLIC_VERCEL_URL + `/api/bookmark/remove/${id}`, {
+    method: 'PUT',
+  });
+  Router.push(`/articles/${id}`);
 }
 
 const Article = (props: Props) => {
@@ -26,6 +41,7 @@ const Article = (props: Props) => {
             {props.isBookmarked ? (
               // ブックマークされている場合には、ブックマークを削除するボタンを設置
               <button
+                onClick={() => removeBookmark(props.article.id)}
                 type='button'
                 className='mt-5 inline-flex items-center rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:hover:bg-red-700 dark:focus:ring-red-800'
               >
@@ -36,6 +52,7 @@ const Article = (props: Props) => {
               </button>
             ) : (
               <button
+                onClick={() => addBookmark(props.article.id)}
                 type='button'
                 className='mt-5 inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
               >
